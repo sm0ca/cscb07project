60ae2f3a81b6da96ac754b90019becf220d0204c
auricz <auric.zhou@gmail.com>
2023-07-23 19:32:09 -0400

    StoreListModel.java now stores a list of StoreListEntry objects

---
+++ app/src/main/java/com/example/mallapp/StoreList/StoreListEntry.java
@@ -0,0 +1,61 @@
+package com.example.mallapp.StoreList;
+
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
+public class StoreListEntry implements Comparable<StoreListEntry> {
+
+    private String storeName;
+    private String logo;
+
+    public StoreListEntry(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public StoreListEntry(String storeName, String imageURL) {
+        this.storeName = storeName;
+        this.logo = imageURL;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StoreListEntry that = (StoreListEntry) o;
+        return storeName.equals(that.storeName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(storeName);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return storeName;
+    }
+
+    @Override
+    public int compareTo(StoreListEntry storeListEntry) {
+        return storeName.compareTo(storeListEntry.getStoreName());
+    }
+
+}
--- app/src/main/java/com/example/mallapp/StoreList/StoreListModel.java
+++ app/src/main/java/com/example/mallapp/StoreList/StoreListModel.java
@@ -17,14 +17,15 @@ import java.util.List;
 
 public class StoreListModel {
 
+    private static final String LOGO_NODE_NAME = "logo";
     private final StoreListPresenter presenter;
     private final DatabaseReference queryNames;
     private ChildEventListener listener;
-    private final List<String> storeNames;
+    private final List<StoreListEntry> stores;
 
     public StoreListModel(StoreListPresenter presenter, String url) {
         this.presenter = presenter;
-        storeNames = new ArrayList<>();
+        stores = new ArrayList<>();
         FirebaseDatabase db = FirebaseDatabase.getInstance(url);
         queryNames = db.getReference().child("stores");
     }
@@ -34,41 +35,64 @@ public class StoreListModel {
         listener = queryNames.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                storeNames.add(snapshot.getKey());
+                String storeName = snapshot.getKey();
+                String logoURL = snapshot.child(LOGO_NODE_NAME).getValue(String.class);
+                StoreListEntry newEntry = new StoreListEntry(storeName, logoURL);
+                if(previousChildName == null) {
+                    stores.add(0, newEntry);
+                }
+                else {
+                    int idxToInsert = stores.indexOf(new StoreListEntry(previousChildName)) + 1;
+                    stores.add(idxToInsert, newEntry);
+                }
+                Log.d("SLM.java", "Prev: " + previousChildName);
                 Log.d("SLM.java", "Added: " + snapshot.getKey());
+                printCurrentList();
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                String newStoreName = snapshot.getKey();
+                String updatedStoreName = snapshot.getKey();
+                String updatedLogo = snapshot.child(LOGO_NODE_NAME).getValue(String.class);
                 if(previousChildName == null) {
-                    storeNames.set(0, newStoreName);
+                    stores.get(0).setStoreName(updatedStoreName);
+                    stores.get(0).setLogo(updatedLogo);
                 }
                 else {
-                    int prevChildIdx = storeNames.indexOf(previousChildName);
-                    storeNames.set(prevChildIdx + 1, newStoreName);
+                    StoreListEntry prev = new StoreListEntry(previousChildName);
+                    int idxToUpdate = stores.indexOf(prev) + 1;
+                    stores.get(idxToUpdate).setStoreName(updatedStoreName);
+                    stores.get(idxToUpdate).setLogo(updatedLogo);
                 }
-                Log.d("SLM.java", "Changed: " + newStoreName);
+                Log.d("SLM.java", "Prev: " + previousChildName);
+                Log.d("SLM.java", "Changed: " + updatedStoreName);
+                printCurrentList();
             }
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                storeNames.remove(snapshot.getKey());
+                String removedStoreName = snapshot.getKey();
+                stores.remove(new StoreListEntry(removedStoreName));
                 Log.d("SLM.java", "Removed: " + snapshot.getKey());
+                printCurrentList();
             }
 
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 String movedName = snapshot.getKey();
-                storeNames.remove(movedName);
+                String movedLogo = snapshot.child(LOGO_NODE_NAME).getValue(String.class);
+                StoreListEntry movedEntry = new StoreListEntry(movedName, movedLogo);
+                stores.remove(new StoreListEntry(movedName));
                 if(previousChildName == null) {
-                    storeNames.add(0, movedName);
+                    stores.add(0, movedEntry);
                 }
                 else {
-                    int prevChildIdx = storeNames.indexOf(previousChildName);
-                    storeNames.add(prevChildIdx + 1, movedName);
+                    int idxToInsert = stores.indexOf(new StoreListEntry(previousChildName)) + 1;
+                    stores.add(idxToInsert, movedEntry);
                 }
+                Log.d("SLM.java", "Prev: " + previousChildName);
                 Log.d("SLM.java", "Moved: " + snapshot.getKey());
+                printCurrentList();
             }
 
             @Override
@@ -79,17 +103,20 @@ public class StoreListModel {
         });
     }
 
+    private void printCurrentList() {
+        Log.d("SLM.java", "========= Printing List =========");
+
+        int i = 0;
+        for(StoreListEntry sle : stores) {
+            Log.d("SLM.java", "idx " + i + ": " + sle.getStoreName());
+            i++;
+        }
+        Log.d("SLM.java", "=================================");
+    }
+
     public void destroyEventListener() {
         if(listener != null) {
             queryNames.removeEventListener(listener);
-        }
-    }
-
-    public void printList() {
-        int i = 0;
-        for(String str : storeNames) {
-            Log.d("SLModel.java List", "idx " + i + ": " + str);
-            i++;
         }
     }
 
