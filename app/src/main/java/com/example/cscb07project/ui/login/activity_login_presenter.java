30d148b4ace00814a39bcdcf03a82eac9a61d46a
auricz <auric.zhou@gmail.com>
2023-08-09 22:04:21 -0400

    made unit tests for login presenter

--- app/src/main/java/com/example/cscb07project/ui/login/activity_login_presenter.java
+++ app/src/main/java/com/example/cscb07project/ui/login/activity_login_presenter.java
@@ -1,9 +1,5 @@
 package com.example.cscb07project.ui.login;
 
-import android.content.Context;
-import android.text.TextUtils;
-import android.widget.Toast;
-
 public class activity_login_presenter implements activity_login_contract.Presenter {
 
     activity_login_contract.View view;
@@ -14,22 +10,25 @@ public class activity_login_presenter implements activity_login_contract.Present
         model = new activity_login_model(this);
     }
 
+    public activity_login_presenter(activity_login_contract.View view, activity_login_contract.Model model) {
+        this.view = view;
+        this.model = model;
+    }
+
     @Override
     public void doLoginEmail(String email, String password) {
         // check if email or password is empty
-        if(TextUtils.isEmpty(email)) {
-            Toast.makeText((Context) view, "Enter your Email", Toast.LENGTH_SHORT).show();
+        if(email.isEmpty()) {
+            doToast("Enter your Email");
             view.progressBarVisibility(4);
             return;
         }
-        if(TextUtils.isEmpty(password)) {
-            Toast.makeText((Context) view, "Enter your Password", Toast.LENGTH_SHORT).show();
+        if(password.isEmpty()) {
+            doToast("Enter your Password");
             view.progressBarVisibility(4);
             return;
         }
-
         model.loggingInUser(email, password);
-
     }
 
     @Override
@@ -51,4 +50,13 @@ public class activity_login_presenter implements activity_login_contract.Present
     public void changeProgressBarVisibility(int mode) {
         view.progressBarVisibility(mode);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        activity_login_presenter that = (activity_login_presenter) o;
+        return view.equals(that.view);
+    }
+
 }
--- app/src/test/java/com/example/cscb07project/ExampleUnitTest.java
+++ app/src/test/java/com/example/cscb07project/ExampleUnitTest.java
@@ -1,17 +1,84 @@
 package com.example.cscb07project;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+
+import com.example.cscb07project.ui.login.activity_login_contract;
+import com.example.cscb07project.ui.login.activity_login_presenter;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+
+    @Mock
+    activity_login_contract.View view;
+    @Mock
+    activity_login_contract.Model model;
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void checkDoLoginEmailEmptyEmail() {
+        activity_login_contract.Presenter presenter = new activity_login_presenter(view, model);
+        presenter.doLoginEmail("", "password");
+        verify(view).doToast("Enter your Email");
+        verify(view).progressBarVisibility(4);
+    }
+
+    @Test
+    public void checkDoLoginEmailEmptyPassword() {
+        activity_login_contract.Presenter presenter = new activity_login_presenter(view, model);
+        presenter.doLoginEmail("bestEmail@hmail.com", "");
+        verify(view).doToast("Enter your Password");
+        verify(view).progressBarVisibility(4);
+    }
+
+    @Test
+    public void checkDoLoginEmailNormal() {
+        activity_login_contract.Presenter presenter = new activity_login_presenter(view, model);
+        presenter.doLoginEmail("e", "p");
+        verify(model).loggingInUser("e", "p");
+    }
+
+    @Test
+    public void checkDoToast() {
+        activity_login_contract.Presenter presenter = new activity_login_presenter(view, model);
+        presenter.doToast("I luv toast");
+        verify(view).doToast("I luv toast");
+    }
+
+    @Test
+    public void checkDoCheckLoggedIn() {
+        activity_login_contract.Presenter presenter = new activity_login_presenter(view, model);
+        presenter.doCheckLoggedIn();
+        verify(model).checkLoggedIn();
+    }
+
+    @Test
+    public void checkIsLoggedIn() {
+        activity_login_contract.Presenter presenter = new activity_login_presenter(view, model);
+        presenter.isLoggedIn();
+        verify(view).loggedIn();
+    }
+
+    @Test
+    public void checkChangeProgressBarVisibility() {
+        activity_login_contract.Presenter presenter = new activity_login_presenter(view, model);
+        presenter.changeProgressBarVisibility(0);
+        verify(view).progressBarVisibility(0);
+    }
+
+    @Test
+    public void checkConstructor() {
+        activity_login_contract.Presenter presenter = new activity_login_presenter(view);
+        assertEquals(presenter, new activity_login_presenter(view));
     }
 }
